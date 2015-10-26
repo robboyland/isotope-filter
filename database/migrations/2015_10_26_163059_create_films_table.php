290f8a442f41477250bdc4cd68219699ae576d85
robboyland <boylandrobert@gmail.com>
2015-10-26 17:03:28 +0000

    add Film model & migration

---
+++ database/migrations/2015_10_26_163059_create_films_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('release_date')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('films');
+    }
+}
